e389d76ef8433806f27a699104a1f3ebaf215e24
mohmoud204 <mohmoud123456789a@gmail.com>
2024-03-23 21:34:35 +0200

    Your commit message here

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CrastsModule } from './crasts/crasts.module';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from "path"
 import { WorkerModule } from './worker/worker.module';
 @Module({
-  imports: [
-      MongooseModule.forRoot('mongodb+srv://mohmoud99:mohmoud99@cluster0.pcyhh7o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'),
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      playground: true,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-    }),
+  imports: [ConfigModule.forRoot(),
+  MongooseModule.forRoot(process.env.DATABASE_URL),
+  GraphQLModule.forRoot<ApolloDriverConfig>({
+    driver: ApolloDriver,
+    playground: true,
+    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+  }),
     CrastsModule,
     WorkerModule],
   controllers: [AppController],
